84a8d3c5d9685eb60026fe96966a4034759478a2
leducnh1290 <leducanh1290@gmail.com>
2025-03-05 16:04:06 +0700

    [add] lab-02 Transposition

--- lab-02/api.py
+++ lab-02/api.py
@@ -3,6 +3,7 @@ from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
 from cipher.railfence import RailFenceCipher
 from cipher.playfair import PlayFairCipher
+from cipher.Transposition import TranspositionCipher
 app = Flask(__name__) 
 #Caesar
 caesar_cipher = CaesarCipher();
@@ -82,5 +83,25 @@ def playfair_decrypt():
     key = data['key']
     playfair_matrix = playfair_cipher.create_playfair_matrix(key)
     return jsonify({'decrypted_text': playfair_cipher.playfair_decrypt(text, playfair_matrix)})
+
+
+#Transposition
+transposition_cipher = TranspositionCipher()
+
+@app.route('/api/transposition/encrypt', methods=['POST'])
+def transposition_encrypt():
+    data = request.get_json()
+    plain_text = data.get('plain_text')
+    key = int(data.get('key'))
+    encrypted_text = transposition_cipher.encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/transposition/decrypt', methods=['POST'])
+def transposition_decrypt():
+    data = request.get_json()
+    cipher_text = data.get('cipher_text')
+    key = int(data.get('key'))
+    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000,debug=True)
