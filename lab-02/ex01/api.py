f0abf35006cf6ef791e54871da94ed672a694305
leducnh1290 <leducanh1290@gmail.com>
2025-03-05 14:36:53 +0700

    [add] lab-02 ex01 caesar

---
+++ lab-02/ex01/api.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from cipher.caesar import CaesarCipher
+app = Flask(__name__) 
+
+caesar_cipher = CaesarCipher();
+@app.route('/api/ceasar/encrypt', methods=['POST'])
+def ceasar_encrypt():
+    data = request.get_json()
+    text = data['plain_text']
+    key = data['key']
+    return jsonify({'encrypted_text': caesar_cipher.encrypt_text(text, key)})
+@app.route('/api/ceasar/decrypt', methods=['POST'])
+def ceasar_decrypt():
+    data = request.get_json()
+    text = data['cipher_text']
+    key = data['key']
+    return jsonify({'decrypted_text': caesar_cipher.decrypt_text(text, key)})
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=5000,debug=True)
